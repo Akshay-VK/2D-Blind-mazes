aea74b86cc13de43793d7f653a367b2bac725386
Akshay-VK <akshaykaruvathil@gmail.com>
2021-04-25 16:39:28 +0530

    added main game loop and tested it ;)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello Github and everyone!');
+const CANVAS: HTMLCanvasElement = document.querySelector('canvas');
+const ctx: CanvasRenderingContext2D = CANVAS.getContext('2d');
+
+const WIDTH: number = 800;
+const HEIGHT: number = 600;
+
+ctx.canvas.width = WIDTH;
+ctx.canvas.height = HEIGHT;
+
+function main(){
+  //MAIN CODE HERE
+
+  //BACKGROUND
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+
+
+  requestAnimationFrame(main);
+
+}
+
+requestAnimationFrame(main);
