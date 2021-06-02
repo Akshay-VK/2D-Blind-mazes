eec2b47ab8da92b06a36656881d62d07a9c61fa2
Akshay-VK <akshaykaruvathil@gmail.com>
2021-06-02 11:09:41 +0530

    turned gameobject class to an interface.. lotta work to do

--- src/game/GameObject.ts
+++ src/game/GameObject.ts
@@ -1,9 +1,11 @@
 import { Dim2 } from "../util/Vectors/Dim2";
+import { BWAnimation } from "../util/BWAnimation";
+import { Canvas } from "../screen/Canvas"
 
-export class GameObject{
+export interface  GameObject{
     dimensions: Dim2;
-    constructor(dim: Dim2){
-	this.dimensions = dim;
-    }
-
+    renderImage: BWAnimation;
+    update(): void;
+    render(ctx: CanvasRenderingContext2D): void;
+    applyToCanvas(cnv: Canvas): Canvas;
 }
